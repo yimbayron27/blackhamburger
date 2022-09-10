329f0a880b792a700c1ebfe98db3ace6aa1b718c
yimbayron27 <110570499+yimbayron27@users.noreply.github.com>
2022-09-09 22:08:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+<script>
+
+
+default function, panel hamburger menu (panelBtn,menulink){
+    const d= document;
+    
+ d.addEventListener("click,e=>"(
+     if(e.target.matches(panelBtn)) || e.target.matches((panelBtn) * )){
+        d.querySelector (panelBtn.classlisten.toggle("is-active");
+     }
+     
+      if (e.target.matches(panelBtn))
+      
+  d.querySelector(panel.classlist.remove(
+    
+</script> 
